7cea6909f4341fd0e546bf110b8fe0d307f2b001
dagash007 <balogunabdulqudusolakunle@gmail.comllllhhhbbbhhshhgggvvvvvkkjlk>
2025-02-19 21:23:51 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Abdul",
+    author_email="olabalo1991@gmail.com",
+    packages=find_packages()
+)
